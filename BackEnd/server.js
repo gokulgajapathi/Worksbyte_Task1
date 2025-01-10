9d079089e51c4957609ba3d72ba6e86886a434c8
Gokul Gajapathi <akgokul22@gmail.com>
2025-01-11 02:47:57 +0530

    commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const dataRoutes = require("./routes/data");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 dot.config();
 const corsOptions = {
   origin: process.env.APP_URL,
